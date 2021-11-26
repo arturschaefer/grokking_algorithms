9bf709e18ad940b3af77925db3051732366e3667
DESKTOP-JFU8E24\Artur Schaefer <artur.schaefer2@gmail.com>
2021-11-27 00:10:19 +0100

    create binary search with Dart

---
+++ 01_introduction_to_algorithms/dart/01_binary_search.dart
@@ -0,0 +1,24 @@
+// Define a function.
+int? binarySearch(List<int> list, int item) {
+  int low = 0;
+  int high = list.length - 1;
+
+  while (low <= high) {
+    int mid = (low + high) ~/ 2;
+    int guess = list[mid];
+    if (guess == item) {
+      return mid;
+    } else if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return null;
+}
+
+void main() {
+  List<int> list = [1, 3, 5, 7, 9];
+  print(binarySearch(list, 3));
+  print(binarySearch(list, -1));
+}
